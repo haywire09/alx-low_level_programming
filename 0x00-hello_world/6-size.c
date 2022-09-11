dabf11a0fb5137f0126bba673c2661a66c98126a
Ayokunle <awhy1_us@hotmail.com>
2022-09-11 16:19:04 -0700

    2

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,24 +1,18 @@
 #include <stdio.h>
 
 /**
- *
- * * main - print the string in the put function
- *
- * *
- *
- * * Description: using the main function
- *
- * * this program prints "Programming is like building a multilingual puzzle
- *
- * * Return: 0
- *
- * */
-int main(void)	
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
+int main(void)
 {
 	char c;
 	int i;
 	long li;
-	long long lli;
+	long lli;
 	float f;
 
 	printf("Size of a char: %ld byte(s)\n", sizeof(c));
@@ -27,5 +21,4 @@ int main(void)
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
 	printf("Size of a float: %ld byte(s)\n", sizeof(f));
 	return (0);
-
-}}
+}
