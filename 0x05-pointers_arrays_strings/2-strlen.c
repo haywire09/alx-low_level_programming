613343ecec012211cc7022fb0e1d110ffc52118b
Ayokunle <awhy1_us@hotmail.com>
2022-09-19 16:48:11 -0700

    2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -12,6 +12,5 @@ int len = 0;
 
 while (*(s + len) != '\0')
 len++;
-
 return (len);
 }
--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,7 +11,6 @@
 void print_rev(char *s)
 {
 int len = strlen(s);
-
 while (len--)
 putchar(*(s + len));
 putchar(10);
