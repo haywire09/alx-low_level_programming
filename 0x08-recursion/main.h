0f69f8a5b75de0a72266d1b1fee4bfb017bb8c74
Ayokunle <awhy1_us@hotmail.com>
2022-09-29 17:07:30 -0700

    2

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -12,5 +13,7 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
+void check(char *s, int start, int end, int *flag);
+int _sqrt(int n, int x);
 
 #endif
