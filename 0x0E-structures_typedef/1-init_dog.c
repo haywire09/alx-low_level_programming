86f29cd52a43872673ca7755389c2cdd9cc420d3
Ayokunle <awhy1_us@hotmail.com>
2022-10-10 17:29:27 -0700

    2

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include "dog.h"
+#include <stdio.h>
 
 /**
  * init_dog - initiliaze a variable of struct type dog
--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include "dog.h"
+#include <stdio.h>
 
 /**
  * free_dog - frees dogs
